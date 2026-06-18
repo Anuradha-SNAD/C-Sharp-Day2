bf683abd382629644fb77255b58e5548c09cba8f
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-06-18 19:55:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Basic_Prgrams.Arrays;
+using Basic_Prgrams.Programs;
+using Basic_Prgrams.Strings;
+//Console.WriteLine("=== Fibonacci ===");
+//Fibonacci.fibonacci();
+//Console.WriteLine();
+//Console.WriteLine("=== Factorial ===");
+//Factorial.factorial();
+//Console.WriteLine("=== Reverse ===");
+//Reverse.reverse();
+//Console.WriteLine();
+//Console.WriteLine("=== To Check polindrome or not  ===");
+//Polidrom.polidrome();
+//Prime.prime();
+//Methods.add(20, 40);
+//Methods.sub(20, 40);
+//Methods.mul(20, 40);
+//Methods.div(20, 40);
+
+//Console.WriteLine("=== Array Programs === ");
+//ArrayPrograms.Display();
+
+//Console.WriteLine();
+//ArrayPrograms.MaxMin();
+//ArrayPrograms.SecondMaxMin();
+//ArrayPrograms.Reverse();
+//ArrayPrograms.Sort();
+//ArrayPrograms.print();
+
+Console.WriteLine("=== String Programs ===");
+StringPrograms.Demo();
+StringPrograms.Reverse();
+StringPrograms.Polindrome();
+StringPrograms.Vowels();
+StringPrograms.CountWords();
+StringPrograms.PrintDuplicates();
